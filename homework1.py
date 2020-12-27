60ff65919e6b3a4f4f7a0fa229196d11f304aa39
ardaefesal <76446797+ardaefesal@users.noreply.github.com>
2020-12-27 18:58:36 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,9 @@
+name = input ("Name: ")
+surname = input("Surname: ")
+age = input ("Age: ")
+country = input ("Country: ")
+mail = input ("Mail Adress: ")
+
+
+
+print(f"Name: {name} \n Surname: {surname} \n Age: {age} \n Country: {country} \n  Mail: {mail}")
