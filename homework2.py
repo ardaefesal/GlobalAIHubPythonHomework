0d4c684fd44137ed50bc08b721abd5f3358f2e42
ardaefesal <76446797+ardaefesal@users.noreply.github.com>
2020-12-27 20:34:32 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,12 @@
+name = input("Name: ")
+surname = input("Surname: ")
+age =int (input("Age: "))
+birthofyear =int(input ("Birth Year: "))
+
+
+
+
+if age <= 18:
+    print("You can't go out because it's too dangerous.")
+else:
+    print("You can go out to the street.")
